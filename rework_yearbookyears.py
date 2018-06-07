23c3149e78de55a340ba40e47f2cc29771ebc633
natolambert <nol5@cornell.edu>
2018-06-07 12:08:41 -0700

    First Commit

---
+++ rework_yearbookyears.py
@@ -0,0 +1,4 @@
+# Script to change the organization and naming convention of the yearbook folders to make the dataset for identifying year rather than M/F
+
+path_orig = 'usr/nato/yearbook_MF'
+path_new = 'usr/nato/yearbook_year'
